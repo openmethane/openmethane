706c77bfb41925af1fbc44dac5d56358e8b29d02
Peter Rayner <prayner@unimelb.edu.au>
2023-05-01 12:18:22 +1000

    version for gadi and pert_pert_test

--- submit.sh
+++ submit.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 # """
 # submit.sh
-# 
+#
 # Copyright 2016 University of Melbourne.
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 # """
-# 
-#SBATCH --partition=physical
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --nodes=1
-#SBATCH --time=1:00:00
-#SBATCH --job-name=fourdvar_main
+#
+#PBS -P q90
+#PBS -q express
+#PBS -N run_pertpert
+#PBS -l walltime=24:00:00,mem=32GB
+#PBS -l ncpus=16
+#PBS -l wd
+source load_p4d_modules.sh
+# replace previous line with whatever you source to run py4dvar
 
-module purge
-module load netCDF
-module load netCDF-Fortran
-module load OpenMPI/3.1.0-iccifort-2018.u4-GCC-8.2.0-cuda9-ucx
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/easybuild/software/netCDF/4.5.0-spartan_intel-2017.u2/lib64"
-
-
-cd /home/stevenpt/fourdvar/py4dvar
-
-python runscript.py >& output.txt
+#python3 restart_script.py
+python3 tests/pert_pert_test.py
