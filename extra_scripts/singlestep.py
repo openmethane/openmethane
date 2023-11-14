8e5f8b906eeb1e91d1908a43c1b3502b77a25371
Peter Rayner <prayner@unimelb.edu.au>
2023-11-15 05:50:35 +1100

    aligning with py4dvar config setup

--- extra_scripts/singlestep.py
+++ extra_scripts/singlestep.py
@@ -1,18 +1,29 @@
-priorFile = '/scratch/q90/sa6589/data/gipnet_methane/archive/real_test/prior.ncf'
-iterFile = '/scratch/q90/cm5310/store_share_p4d/archive/example_experiment/iter0023.ncf'
-obsFile = '/scratch/q90/sa6589/data/gipnet_methane/archive/real_test/observed.pickle'
-simulFile = '/scratch/q90/cm5310/plotting/simulations.pickle'
+"""
+singlestep.py
+
+Copyright 2023 The Superpower Institute Ltd
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+"""
+
+import context
 import fourdvar.user_driver as user
 import fourdvar.datadef as d
 from fourdvar._transform import transform
-
-observed = d.ObservationData.from_file( obsFile)
-physical = d.PhysicalData.from_file( priorFile)
-#simul = d.ObservationData.from_file( simulFile )
+from fourdvar.params.root_path_defn import store_path
+import os
+import sys
+from fourdvar.params.input_defn import prior_file, obs_file
+physical = d.PhysicalData.from_file( prior_file)
 modelInput = transform( physical, d.ModelInputData)
-#modelInput.archive('/scratch/q90/cm5310/plotting/model_input')
 modelOutput = transform( modelInput, d.ModelOutputData)
+observed = d.ObservationData.from_file( obs_file)
+#simul = d.ObservationData.from_file( simulFile )
+#modelInput.archive('/scratch/q90/cm5310/plotting/model_input')
 simul = transform( modelOutput, d.ObservationData)
+simul.archive( os.path.join(store_path, 'simulobs.pic.gz'))
 residual = d.ObservationData.get_residual(observed, simul)
 #residual.archive('/scratch/q90/cm5310/plotting/residual')
 #simul.archive('/scratch/q90/cm5310/plotting/simulations.pickle')
