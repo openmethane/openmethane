976af49a48e5bfd839f1fc9d9be74819fd13fb92
Jared Lewis <jared@jared.kiwi.nz>
2024-06-05 19:02:19 +0300

    fix: Add test for make_prior

---
+++ tests/integration/cmaq_preprocess/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from scripts.cmaq_preprocess.make_emis_template import make_emissions_templates
+
+
+@pytest.fixture
+def emission_template(test_data_dir, tmpdir):
+    data_dir = tmpdir.mkdir("data")
+
+    emis_template = str(data_dir / "emis_record_<YYYY-MM-DD>.nc")
+    make_emissions_templates(
+        prior_filename=str(test_data_dir / "prior" / "out-om-domain-info.nc"),
+        metcro_template=str(test_data_dir / "mcip" / "METCRO3D_220701_aust-test"),
+        emis_template=emis_template,
+    )
+
+
+    yield emis_template
+
