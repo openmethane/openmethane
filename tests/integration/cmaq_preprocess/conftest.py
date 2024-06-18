91382689b224a6b21a1d2db717b0abb9f65d1247
Jared Lewis <jared@jared.kiwi.nz>
2024-06-18 13:14:11 +1000

    fix: Path to METCRO data

--- tests/integration/cmaq_preprocess/conftest.py
+++ tests/integration/cmaq_preprocess/conftest.py
@@ -9,7 +9,9 @@ def emission_template(test_data_dir, tmpdir):
     emis_template = str(data_dir / "emis_record_<YYYY-MM-DD>.nc")
     make_emissions_templates(
         prior_filename=str(test_data_dir / "prior" / "out-om-domain-info.nc"),
-        metcro_template=str(test_data_dir / "mcip" / "METCRO3D_220701_aust-test"),
+        metcro_template=str(
+            test_data_dir / "mcip" / "2022-07-22" / "d01" / "METCRO3D_220701_aust-test"
+        ),
         emis_template=emis_template,
     )
 
