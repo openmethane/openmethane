1cab297a253f338ce991f7f86f0d6704d4aa02d0
Peter Rayner <prayner@unimelb.edu.au>
2024-07-01 11:10:07 +1000

    Adding date templates for icon and bcon

--- fourdvar/params/cmaq_config.py
+++ fourdvar/params/cmaq_config.py
@@ -140,8 +140,8 @@ fwd_xfirst_file = os.path.join( output_path, 'XFIRST.<YYYYMMDD>' )
 bwd_xfirst_file = os.path.join( output_path, 'XFIRST.bwd.<YYYYMMDD>' )
 
 #input files
-icon_file = os.path.join( icon_path, 'icon.nc' )
-bcon_file = os.path.join( bcon_path, 'bcon.nc')
+icon_file = os.path.join( icon_path, 'ICON.d01.o.CH4only.nc' )
+bcon_file = os.path.join( bcon_path, 'BCON.d01.o.CH4only.nc')
 emis_file = os.path.join( emis_path, 'emis.<YYYY-MM-DD>.nc' )#Shak
 force_file = os.path.join( cmaq_base,  'force', 'ADJ_FORCE.<YYYYMMDD>.nc' )
 #required met data, use unknown #?????
