5366877ce8edf3c1ab35ba1e8e6fa8d2d6e724af
Jared Lewis <jared@jared.kiwi.nz>
2024-07-05 11:02:31 +1000

    chore: Add load_p4d_modules.sh to git

---
+++ load_p4d_modules.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Loads the required modules on Gadi for running openmethane.
+#
+# This should be sourced prior to running any of the openmethane scripts.
+
+#cmaq-stuff
+module purge
+module load pbs
+module load intel-compiler/2019.3.199
+module load openmpi/4.0.3
+module load netcdf/4.7.1
+module load hdf5/1.10.5
+module load nco
+
+#python-stuff
+module load python3/3.11.7
+[[ ":$PATH:" != *":$HOME/poetry/bin:"* ]] && export PATH="$HOME/poetry/bin:${PATH}"
