9d8335c36de7ea8ded65de1ca53af4224ea37f3a
Jared Lewis <jared@jared.kiwi.nz>
2024-08-18 07:22:13 +0200

    fix: Check directory exists

--- scripts/load_from_archive.py
+++ scripts/load_from_archive.py
@@ -30,28 +30,48 @@ END_DATE = env.date("END_DATE")
 def main():
     for date in date_range(START_DATE, END_DATE):  # this is inclusive of END_DATE
         for path in [
-            [
-                "input",
-                "test_obs.pic.gz",
-            ],
+            ["input"],
             ["mcip"],
         ]:
-            _sync_daily_glob(date, path)
+            _sync_daily_directory(date, path)
 
 
-def _sync_daily_glob(date: datetime.date, path: list[str]):
+def _sync_daily_directory(date: datetime.date, path: list[str]):
+    """
+    Sync output from an archive of a successful daily workflow.
+
+    Parameters
+    ----------
+    date
+        Date to sync
+    path
+        Relative path to a directory within the daily results to fetch.
+
+        This doesn't currently fetch individual files,
+        instead an entire directory including its subdirectories are fetched.
+    """
     daily_prefix = (
         DOMAIN_NAME,
         "daily",
         str(date.year),
-        str(date.month),
-        str(date.day),
+        f"{date.month:02}",
+        f"{date.day:02}",
     )
-    s3_path = "/".join((TARGET_BUCKET, *daily_prefix, *path))
+    s3_path = "/".join((TARGET_BUCKET, *daily_prefix, *path)).rstrip("/")
+    s3_path = s3_path + "/"  # S3 directory paths must end with a slash
+
+    # Verify that the path exists
+    # Raises CalledProcessError if the path doesn't exist
+    subprocess.run(["aws", "s3", "ls", s3_path], check=True, capture_output=False)
+
     local_path = STORE_PATH.joinpath(*daily_prefix, *path)
     local_path.mkdir(parents=True, exist_ok=True)
-    logging.debug(f"Downloading {s3_path} to {local_path}")
-    subprocess.run(["aws", "s3", "sync", "--no-progress", s3_path, str(local_path)], check=True)
+    logging.info(f"Downloading {s3_path} to {local_path}")
+
+    # Download the path
+    command = ["aws", "s3", "sync", "--no-progress", s3_path, str(local_path)]
+    res = subprocess.run(command, check=True, capture_output=True, text=True)
+    logging.debug(f"Output from {' '.join(command)!r}: {res.stdout}")
 
 
 def date_range(start_date: datetime.date, end_date: datetime.date):
