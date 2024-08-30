4a4129e7a3721b4e436ceae89410737b7ca0010f
Mika Pflüger <mika.pflueger@climate-resource.com>
2024-08-30 11:33:28 +0200

    test: use the existing test files

--- tests/unit/cmaq_preprocess/test_bias_correct_cams.py
+++ tests/unit/cmaq_preprocess/test_bias_correct_cams.py
@@ -13,20 +13,19 @@ import cmaq_preprocess.utils
 
 def test_bias_zero_after_correct(test_data_dir, tmp_path, monkeypatch, metcro3d_file):
     # setup test data
-    start_date = datetime.date(2022, 7, 23)
-    end_date = datetime.date(2022, 7, 23)
-    monkeypatch.setenv("START_DATE", "2022-07-23")
-    monkeypatch.setenv("END_DATE", "2022-07-23")
+    start_date = datetime.date(2022, 7, 22)
+    end_date = datetime.date(2022, 7, 22)
+    monkeypatch.setenv("START_DATE", "2022-07-22")
+    monkeypatch.setenv("END_DATE", "2022-07-22")
 
-    # TODO: test data still missing
-    icon_file_name = "ICON.d??.aust-test_v1.CH4only.nc"
+    icon_file_name = "template_icon_profile_CH4only_d01.nc"
     icon_file_src = test_data_dir / "cmaq" / icon_file_name
     icon_file = tmp_path / icon_file_name
     shutil.copy(icon_file_src, icon_file)
-    bcon_file_names = ["BCON.d??.aust-test_v1.CH4only.nc"]
+    bcon_file_names = ["template_bcon_profile_CH4only_d01.nc"]
     bcon_files_src = [test_data_dir / "cmaq" / n for n in bcon_file_names]
     bcon_files = [tmp_path / n for n in bcon_file_names]
-    for i, fname in bcon_files:
+    for i, fname in enumerate(bcon_files):
         shutil.copy(bcon_files_src[i], fname)
     obs_file = test_data_dir / "obs" / "test_obs_2022-07-22.pic.gz"
 
@@ -41,7 +40,9 @@ def test_bias_zero_after_correct(test_data_dir, tmp_path, monkeypatch, metcro3d_
         end_date=end_date,
     )
 
-    assert bias != 0.0
+    # pre-existing bias has to be larger than almost zero, otherwise the later
+    # assert is meaningless
+    assert abs(bias) > 1e-6
 
     # correct bias
     cmaq_preprocess.bias.correct_icon_bcon(
@@ -59,4 +60,4 @@ def test_bias_zero_after_correct(test_data_dir, tmp_path, monkeypatch, metcro3d_
         start_date=start_date,
         end_date=end_date,
     )
-    assert new_bias == 0.0
+    assert abs(new_bias) <= 1e-6
