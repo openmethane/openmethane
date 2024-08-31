27c1be3bd5dc7fecbf4b7f4c58b418bb5ad137cf
Peter Rayner <prayner@unimelb.edu.au>
2024-08-31 17:58:51 +1000

    adding retrieval of solution or best iteration

--- src/postproc/calculate_average_emissions.py
+++ src/postproc/calculate_average_emissions.py
@@ -5,19 +5,25 @@ import glob
 
 import numpy as np
 import xarray as xr
+from netCDF4 import Dataset
 
 from fourdvar.datadef.observation_data import ObservationData
 from fourdvar.params.input_defn import obs_file
 from fourdvar.params.template_defn import template_dir
 SPECIES_MOLEMASS = {'CH4': 16} # molar mass in gram
 G2KG = 1e-3 # conv factor kg to g
-def calculate_average_emissions( output_file: pathlib.Path,
+SOLUTION_FILENAME = 'final_solution.ncf'
+def calculate_average_emissions(
+        archive_dir: pathlib.Path,
+                                 output_file: pathlib.Path,
                                  emis_template: str = 'emis_*.nc',
+                                 iter_template: str = 'iter*.ncf',
                                  species: str = 'CH4',
                                  delete_attrs: list|tuple = ['NLAYS', 'NVARS',],
-                                 override_attrs= None, 
+                                 override_attrs= None,
+                                 designated_posterior_file: pathlib.Path = None,
                                 ):
-    prior_emis_files = list_emis_template_files(emis_template)
+    prior_emis_files = list_emis_template_files( template_dir, emis_template)
     prior_emis_list = []
     for filename in prior_emis_files:
         with xr.open_dataset( filename) as xrds:
@@ -25,7 +31,8 @@ def calculate_average_emissions( output_file: pathlib.Path,
     prior_emis_array = np.array( prior_emis_list)
     prior_emis_mean_3d = prior_emis_array.mean(axis=(0,1))
     prior_emis_mean_surf = prior_emis_mean_3d[0,...]
-    posterior_multiplier = get_posterior()
+    posterior_multiplier = get_posterior( archive_dir, species,
+                                          iter_template, designated_posterior_file)
     posterior_emis_mean_surf = posterior_multiplier * prior_emis_mean_surf
     # create output based on an emis file input
     with xr.open_dataset( prior_emis_files[0]) as in_ds:
@@ -37,13 +44,48 @@ def calculate_average_emissions( output_file: pathlib.Path,
         # now create coordinates, missing from input
         x = in_ds.XORIG + 0.5*in_ds.XCELL + np.arange(in_ds.NCOLS) * in_ds.XCELL
         y = in_ds.YORIG + 0.5*in_ds.YCELL + np.arange( in_ds.NROWS) * in_ds.YCELL
-        posterior_emis_mean_xr = xr.DataArray( posterior_emis_mean_output, coords={'x':x, 'y':y})
+        posterior_emis_mean_xr = xr.DataArray( posterior_emis_mean_output, coords={'y':y, 'x':x})
         posterior_emis_mean_xr.attrs['units'] = 'kg/m**2/s'
         out_ds[species] = posterior_emis_mean_xr
         out_ds.to_netcdf(output_file)
     return
 
-def get_posterior(): return np.ones((5,5))
+def get_posterior(archive_dir: pathlib.Path,
+                  species: str = 'CH4',
+                  iter_template: str = 'iter*.ncf',
+                  designated_posterior_file : pathlib.Path = None,
+                  ) -> np.ndarray:
+    if designated_posterior_file is not None:
+        posterior_file = designated_posterior_file
+    else:
+        posterior_file = find_last_iteration( archive_dir, iter_template)
+    with  Dataset( posterior_file) as posterior_nc:
+        emis_group = posterior_nc['/emis']
+        emis_array = emis_group[species][...]
+    # we can't assume how many dimensions this will have, preserve the last two and average over all the rest
+    PRESERVED_DIMENSIONS = 2
+    if emis_array.ndim > 2:
+        averaged_dimensions = emis_array.ndim - PRESERVED_DIMENSIONS
+        averaged_axes = tuple(range( averaged_dimensions))
+        result = emis_array.mean(axis=averaged_axes)
+    else:
+        result = emis_array
+    return result
+def find_last_iteration( archive_dir: pathlib.Path,
+                         iter_template: str) -> pathlib.Path:
+    """ returns successful convergence output if present, otherwise last iteration """
+    solution_path = archive_dir / SOLUTION_FILENAME
+    if solution_path.is_file():
+        result = solution_path
+    else:
+        iter_glob = archive_path / iter_template
+        iter_files = glob.glob( str(iter_glob))
+        if iter_files is None:
+            raise ValueError(f'no converged iterations found at {iter_glob}')
+        else:
+            iter_files.sort()
+            result =  iter_files[-1]
+    return result
 def copy_attributes( in_ds: xr.Dataset,
                      out_ds: xr.Dataset,
                      override_attrs = None,
@@ -62,7 +104,9 @@ def copy_attributes( in_ds: xr.Dataset,
     
                      
                                 
-def list_emis_template_files( emis_template: str) -> list:
+def list_emis_template_files(template_dir: pathlib.Path,
+                             emis_template: str,
+                             ) -> list:
     record_dir = template_dir / 'record'
     prior_emis_glob = record_dir / emis_template
     prior_emis_files = glob.glob( str(prior_emis_glob))
