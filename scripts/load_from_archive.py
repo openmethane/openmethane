5207466bfd2b088cdc78f09a7b37db7ea404c9dd
Lindsay Gaines <lindsay.gaines@superpowerinstitute.com.au>
2025-01-16 11:11:58 +1100

    Support destination path in load_from_archive
    This is a non-functional change for loading the daily data into a monthly run working folder, but it allows us to adapt this script for loading a subset of archived daily data into a new daily run for the same day, which we'll use to avoid expensive re-processing of WRF result data.

--- scripts/load_from_archive.py
+++ scripts/load_from_archive.py
@@ -27,17 +27,25 @@ START_DATE = env.date("START_DATE")
 END_DATE = env.date("END_DATE")
 
 
-def main():
+def monthly():
     for date in date_range(START_DATE, END_DATE):  # this is inclusive of END_DATE
+        daily_destination_path = pathlib.Path(
+            DOMAIN_NAME,
+            "daily",
+            str(date.year),
+            f"{date.month:02}",
+            f"{date.day:02}",
+        )
+
         for path in [
             ["input"],
             ["cmaq"],
             ["mcip"],
         ]:
-            _sync_daily_directory(date, path)
+            _sync_daily_directory(date, path, daily_destination_path)
 
 
-def _sync_daily_directory(date: datetime.date, path: list[str]):
+def _sync_daily_directory(date: datetime.date, path: list[str], destination: pathlib.Path):
     """
     Sync output from an archive of a successful daily workflow.
 
@@ -51,21 +59,21 @@ def _sync_daily_directory(date: datetime.date, path: list[str]):
         This doesn't currently fetch individual files,
         instead an entire directory including its subdirectories are fetched.
     """
-    daily_prefix = (
+    daily_archive_path = (
         DOMAIN_NAME,
         "daily",
         str(date.year),
         f"{date.month:02}",
         f"{date.day:02}",
     )
-    s3_path = "/".join((TARGET_BUCKET, *daily_prefix, *path)).rstrip("/")
+    s3_path = "/".join((TARGET_BUCKET, *daily_archive_path, *path)).rstrip("/")
     s3_path = s3_path + "/"  # S3 directory paths must end with a slash
 
     # Verify that the path exists
     # Raises CalledProcessError if the path doesn't exist
     subprocess.run(["aws", "s3", "ls", s3_path], check=True, capture_output=False)
 
-    local_path = STORE_PATH.joinpath(*daily_prefix, *path)
+    local_path = STORE_PATH.joinpath(destination, *path)
     local_path.mkdir(parents=True, exist_ok=True)
     logging.info(f"Downloading {s3_path} to {local_path}")
 
@@ -83,4 +91,4 @@ def date_range(start_date: datetime.date, end_date: datetime.date):
 
 if __name__ == "__main__":
     logging.basicConfig(level=env.str("LOG_LEVEL", "DEBUG"))
-    main()
+    monthly()
