8ae11c2bbd44b0518d608c3191cc5e329c46f5c8
Peter Rayner <prayner@unimelb.edu.au>
2026-09-01 09:16:13 +1000

    fixing header to acknowledge Nesser

--- src/openmethane/obs_preprocess/column_operator.py
+++ src/openmethane/obs_preprocess/column_operator.py
@@ -1,18 +1,6 @@
-#
-# Copyright 2016 University of Melbourne.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# adapted from python/TROPOMI_operator.py from https://github.com/hannahnesser/TROPOMI_inversion
+# published in https://doi.org/10.5194/acp-24-5069-2024
+
 """Vertical part of the TROPOMI CH4 column observation operator.
 
 The operational TROPOMI CH4 retrieval reports a column-averaged dry-air mole
